bef5ddd2a74a8280bd03a7cd0117382b305c20c4
Boubacar Diallo <boubabauel@gmail.com>
2022-03-23 17:04:04 -0400

    new

---
+++ mnew/script.js
@@ -0,0 +1,5 @@
+// const hambuger = document.getElementById('hambuger');
+// const navUL = document.getElementById('nav-ul');
+// hambuger.addEventListener('click',()=>{
+//     navUL.classList.toggle('show');
+// });
