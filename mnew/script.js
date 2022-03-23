bc81ed77b54073907e0e6390ff8778eb86212ba4
Boubacar Diallo <90484140+boubabaueel@users.noreply.github.com>
2022-03-23 18:36:42 -0400

    Delete mnew directory

--- mnew/script.js
+++
@@ -1,5 +0,0 @@
-// const hambuger = document.getElementById('hambuger');
-// const navUL = document.getElementById('nav-ul');
-// hambuger.addEventListener('click',()=>{
-//     navUL.classList.toggle('show');
-// });
